e04df9a50571d3e18b78dadbb4e1a7a9caa526c0
alberto <alberto.scaringi@abstract.it>
2021-12-14 15:36:46 +0100

    primo commit

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+import { Divider, Text } from 'react-native-paper';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class Touchables extends Component {
+  _onPressButton() {
+    alert('You tapped the button!')
+  }
+
+  _onLongPressButton() {
+    alert('You long-pressed the button!')
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+            <Text style={styles.buttonText}>Reggia di caserta</Text>
+        </TouchableHighlight>
+
+        <Divider style={styles.divider}/>
+        <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+            <Text style={styles.buttonText}>Piazza Vanvitelli</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 60,
+    alignItems: 'center'
   },
+  buttonText: {
+    textAlign: 'center',
+    padding: 20,
+    color: 'red',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
+  },
+  divider: {
+    color: 'red'
+  }
 });
